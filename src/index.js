2742ca6770e7d66e1a09968633eccf17e0353bcb
nqvtech-it <111405183+nqvtech-it@users.noreply.github.com>
2023-02-27 16:50:38 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+require("../node_modules/react-virtual-keyboard/lib/scss/style.scss");
+require("./scss/style.scss");
+
+ReactDOM.render(<App />, document.getElementById("root"));
